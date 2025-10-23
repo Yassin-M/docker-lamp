f511268f125870b633d56919fd0a0f0ee6be0699
asiiersc <asier08032010@gmail.com>
2025-10-23 10:40:16 +0200

    add_item DB insert comprobatu

--- app/config/add_item.php
+++ app/config/add_item.php
@@ -1,12 +1,12 @@
 <?php
    include("../index.php");
-   $item_izena = $_POST['item_izena'];
-   $item_kostua = $_POST['item_kostua'];
-   $item_bizitza = $_POST['item_bizitza'];
-   $item_erasoa = $_POST['item_erasoa'];
-   $item_mota = $_POST['item_mota'];
+   $item_izena = $_POST['karta_izena'];
+   $item_kostua = $_POST['karta_kostua'];
+   $item_bizitza = $_POST['karta_bizitza'];
+   $item_erasoa = $_POST['karta_erasoa'];
+   $item_mota = $_POST['karta_mota'];
 
-   $sql = "INSERT INTO items (izena, kostua, bizitza, erasoa, mota) VALUES ('$item_izena', '$item_kostua', '$item_bizitza', '$item_erasoa', '$item_mota')";
+   $sql = "INSERT INTO Datuak (izena, kostua, bizitza, erasoa, mota) VALUES ('$item_izena', '$item_kostua', '$item_bizitza', '$item_erasoa', '$item_mota')";
 
    if ($conn->query($sql) === TRUE) {
       echo "Item added successfully.";
