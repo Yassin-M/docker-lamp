6e94b9294694f9492e222653332529585c5f7700
Ander <ander.ibarra.garnika@gmail.com>
2025-11-14 18:50:44 +0100

    Azken zuzenketak

--- app/config/csrf.php
+++ app/config/csrf.php
@@ -13,7 +13,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-// Ensure security headers are present for direct requests to this script
 if (file_exists(__DIR__ . '/headers.php')) {
     include_once __DIR__ . '/headers.php';
 }
